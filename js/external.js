269a46e5591f4c82273ee0bb6c39d2c27fbaa5e5
SgtMerrell <74498850+SgtMerrell@users.noreply.github.com>
2022-11-21 12:38:33 -0600

    adding in javascript with html lessons

---
+++ js/external.js
@@ -0,0 +1,7 @@
+console.log("Hello from external JavaScript");
+
+// alert("Welcome to my Website!");
+
+let favColor = prompt("What is your favorite color?");
+let message = "Great, " + favColor + " is my favorite color too";
+alert(message);
