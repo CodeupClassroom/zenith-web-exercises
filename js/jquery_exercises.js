3353067dccf8e5205a99fa6733e0786dbc6159f9
SgtMerrell <74498850+SgtMerrell@users.noreply.github.com>
2023-01-23 08:50:47 -0600

    mapbox

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,13 +1,30 @@
 $(document).ready(function(){
-    // document.querySelectorAll('.login').forEach(function(element){
-    //     element.addEventListener('click', function(){
-    //         this.setAttribute('style', 'background-color: red');
-    //     });
-    // });
-    $('.login').click(function(){
-        $(this).css('background-color', 'red');
-    });
     $('.menu, .close-menu').click(function(){
        $('.mobile-menu').toggleClass('open');
     });
+     $('#filter').on('keyup', function(event){
+    //document.querySelector('#filter').addEventListener('keyup', function(event){
+        let keyPressed = event.keyCode;
+        if (keyPressed === 27) {
+            $(this).val('');
+        }
+        let text = $(this).val();
+        if (text === '') {
+            $('li').show();
+        } else {
+            $('li').each(function () {
+                let liText = $(this).text();
+                if (text.toLowerCase() === liText.toLowerCase()) {
+                    $(this).show();
+                } else {
+                    $(this).hide();
+                }
+            });
+        }
+    });
+     $(document).on('click', 'li', function(){
+        $(this).css('color', 'red');
+     });
+     let newListItem = `<li class="codeup">New dynamically added li</li>`;
+     $('#list2').append(newListItem);
 });
